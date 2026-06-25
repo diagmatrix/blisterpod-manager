ccd8085cffd9b63cfb56d3a539ab57ae0dc70bd6
diagmatrix <manuelgbal1@gmail.com>
2026-06-25 23:57:11 +0200

    Fix Add Cards batch panel layout

--- src/renderer/src/components/Layout.tsx
+++ src/renderer/src/components/Layout.tsx
@@ -6,8 +6,8 @@ export default function Layout() {
   return (
     <SidebarProvider defaultOpen={false}>
       <AppSidebar />
-      <SidebarInset>
-        <div className="flex-1 overflow-y-auto p-4">
+      <SidebarInset className="h-svh overflow-hidden">
+        <div className="flex-1 min-h-0 overflow-y-auto p-4">
           <Outlet />
         </div>
       </SidebarInset>
